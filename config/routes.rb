020ebdfe4fd7b4152e4c0ef3395f6a344171cb8a
keiyan9 <nanbakouichi@nanba-no-MacBook-Air.local>
2014-03-15 22:35:26 +0900

    add home view

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 TweetMemories::Application.routes.draw do
+
+  resources :memories, :only => %w(index)
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'memories#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
